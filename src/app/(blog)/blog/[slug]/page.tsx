9d453aa28a6d247fce4749cf03e79c299b351255
Tubolayefa <tlayefaah@gmail.com>
2025-01-04 21:38:30 +0000

    update views on load page

--- src/app/(blog)/blog/[slug]/page.tsx
+++ src/app/(blog)/blog/[slug]/page.tsx
@@ -1,4 +1,3 @@
-/* eslint-disable @next/next/no-img-element */
 "use client";
 import { client } from "@/sanity/lib/client";
 import { POST_QUERY, POSTS_QUERY } from "@/sanity/lib/queries";
@@ -12,74 +11,86 @@ import GoBack from "@/components/ui/GoBack";
 import { urlFor } from "@/sanity/lib/image";
 import PostCard from "@/components/ui/PostCard";
 import { MdShare } from "react-icons/md";
+import { writeClient } from "@/sanity/lib/write-client";
 
 const Page = () => {
-  const [posts, setPosts] = useState<Post[] | null>(null); // Type posts as array or null
+  const [posts, setPosts] = useState<Post[] | null>(null);
   const [loading, setLoading] = useState<boolean>(true);
   const [error, setError] = useState<string | null>(null);
-  const [post, setPost] = useState<Post | null>(null); // Handle the post state as Post | null
+  const [post, setPost] = useState<Post | null>(null);
   const { slug: $slug } = useParams();
   const md = markdownit();
-
-  const parsedContent = md.render(post?.post || "");
-
-  //   console.log($id);
-
-  const fetchPost = async () => {
-    try {
-      const postData = await client.fetch(POST_QUERY, { slug: $slug });
-      // console.log("Fetched Post:", JSON.stringify(postData, null, 2));
-
-      setPost(postData || null); // Ensure the post is set or null if not found
-    } catch (error) {
-      console.error("Error fetching post:", error);
-      setPost(null); // Set to null in case of error
-    }
-  };
-
-  const fetchPosts = async () => {
-    try {
-      setLoading(true);
-      const posts = await client.fetch(POSTS_QUERY);
-      // console.log("Fetched Posts:", JSON.stringify(posts, null, 2));
-      setPosts(posts); // Set posts as raw data
-    } catch (error) {
-      console.error("Error fetching posts:", error);
-      setError("Failed to fetch posts");
-    } finally {
-      setLoading(false);
-    }
-  };
+  console.log(post?.views)
 
   useEffect(() => {
+    const fetchPost = async () => {
+      try {
+        const postData = await client.fetch(POST_QUERY, { slug: $slug });
+        setPost(postData || null);
+      } catch (error) {
+        console.error("Error fetching post:", error);
+        setPost(null);
+      }
+    };
+
+    const fetchPosts = async () => {
+      try {
+        setLoading(true);
+        const posts = await client.fetch(POSTS_QUERY);
+        setPosts(posts);
+      } catch (error) {
+        console.error("Error fetching posts:", error);
+        setError("Failed to fetch posts");
+      } finally {
+        setLoading(false);
+      }
+    };
+
     fetchPosts();
-  }, []);
+    fetchPost();
+  }, [$slug]);
 
   useEffect(() => {
-    fetchPost();
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [$slug]); // Fetch post when the ID changes
+    const updateViews = async () => {
+      if (!post) return;
+
+      const storedIds = JSON.parse(localStorage.getItem("postIds") || "[]");
+      if (storedIds.includes(post._id)) return;
+
+      localStorage.setItem("postIds", JSON.stringify([...storedIds, post._id]));
+
+      try {
+        await writeClient
+          .patch(post._id)
+          .setIfMissing({ views: 0 })
+          .inc({ views: 1 })
+          .commit();
+        console.log("Views updated successfully");
+      } catch (error) {
+        console.error("Error updating views:", error);
+      }
+    };
+
+    updateViews();
+  }, [post]);
 
   if (!post) {
     return (
       <div className="mt-64">
         <Loader />
       </div>
-    ); // Display loading state or error message
+    );
   }
 
   const renderPosts = posts
-    ?.filter((p) => p._id !== post?._id) // Exclude the current post
-    .sort(() => Math.random() - 0.5) // Shuffle the posts array
-    .slice(0, 4) // Take the first 4 items
+    ?.filter((p) => p._id !== post?._id)
+    .sort(() => Math.random() - 0.5)
+    .slice(0, 4)
     .map((p) => <PostCard key={p._id} post={p} />);
 
-  // SHARE
-  // SHARE
   const handleShare = () => {
-    const url = window.location.href; // Get the current URL
+    const url = window.location.href;
     if (navigator.share) {
-      // Use the Web Share API if available
       navigator
         .share({
           title: "Check out this blog post!",
@@ -89,7 +100,6 @@ const Page = () => {
         .then(() => console.log("Content shared successfully"))
         .catch((error) => console.error("Error sharing content", error));
     } else {
-      // Fallback: Copy the URL to the clipboard
       navigator.clipboard
         .writeText(url)
         .then(() => alert("URL copied to clipboard"))
@@ -98,23 +108,19 @@ const Page = () => {
   };
 
   return (
-    // <div className="pt-[76px] pb-20">
-    <div className="max-md:pt-16  pt-[77px] pb-20 ">
+    <div className="max-md:pt-16 pt-[77px] pb-20 ">
       <div
         className="h-[20vh] sm:h-[35vh] flex flex-col pt-3 sm:pt-7"
         style={{
           backgroundImage: post?.image
             ? `linear-gradient(to bottom, rgba(0, 0, 0, 0.7), rgba(0, 0, 0, 0.7)), url(${urlFor(post?.image).url()})`
-            : "linear-gradient(to bottom, rgba(0, 0, 0, 0.7), rgba(0, 0, 0, 0.7)), url('/default-image.jpg')", // Fallback to a default image if post.image is not available
+            : "linear-gradient(to bottom, rgba(0, 0, 0, 0.7), rgba(0, 0, 0, 0.7)), url('/default-image.jpg')",
           backgroundRepeat: "no-repeat",
           backgroundPosition: "center",
           backgroundSize: "cover",
-          // display: "flex",
-          // justifyContent: "center",
-          // alignItems: "center",
         }}
       >
-        <div className="flex justify-between px-4 sm:px-8  text-white w-full container mx-auto">
+        <div className="flex justify-between px-4 sm:px-8 text-white w-full container mx-auto">
           <GoBack />
           <div className="flex items-center space-x-3">
             <button onClick={handleShare} aria-label="Share">
@@ -126,16 +132,16 @@ const Page = () => {
           {capitalizeWords(post?.title)}
         </h1>
       </div>
-      {/* </div> */}
 
       <div className="px-4 md:px-8 pt-10 container">
         <section className="mb-20 text-justify">
-          {parsedContent ? (
-            <article dangerouslySetInnerHTML={{ __html: parsedContent }} />
+          {md.render(post?.post || "") ? (
+            <article
+              dangerouslySetInnerHTML={{ __html: md.render(post?.post || "") }}
+            />
           ) : (
             <p>No details Provided</p>
           )}
-
           <p className="mt-7 text-gray-300 text-sm">
             {post?._createdAt && formatDate(post?._createdAt)}
           </p>
@@ -145,13 +151,13 @@ const Page = () => {
           <h3 className="text-xl font-medium mb-5 text-start">More Reads</h3>
           <div className="flex gap-10 overflow-auto pb-10">
             {loading ? (
-              <div className=" mt-36 w-screen flex justify-center ">
+              <div className="mt-36 w-screen flex justify-center">
                 <Loader />
-              </div> // Loading state
+              </div>
             ) : error ? (
-              <div>{error}</div> // Error state
+              <div>{error}</div>
             ) : (
-              renderPosts // Render posts when data is available
+              renderPosts
             )}
           </div>
         </section>
