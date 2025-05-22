944106f74f602990a526cc898b10d6c9bcfa74ec
Tubolayefa <tlayefaah@gmail.com>
2025-05-22 13:30:03 +0100

    commented speakers

--- src/app/(event)/events/big-conference/page.tsx
+++ src/app/(event)/events/big-conference/page.tsx
@@ -2,7 +2,7 @@ import About from "@/components/bigConference/About";
 import Faq from "@/components/bigConference/Faq";
 import Hero from "@/components/bigConference/Hero";
 import Partners from "@/components/bigConference/Partners";
-import Speakers from "@/components/bigConference/Speakers";
+// import Speakers from "@/components/bigConference/Speakers";
 import WhatElse from "@/components/bigConference/WhatElse";
 import React from "react";
 
