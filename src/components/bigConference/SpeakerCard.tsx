73b2ae6c0c3ed2ea472e0d64b30414a809b8a00f
Tubolayefa <tlayefaah@gmail.com>
2025-07-28 00:11:30 +0100

    adjust

--- src/components/bigConference/SpeakerCard.tsx
+++ src/components/bigConference/SpeakerCard.tsx
@@ -12,7 +12,7 @@ const SpeakerCard = ({
 }) => {
   return (
     <div className="flex flex-col items-center gap-4">
-      <div className="overflow-hidden  w-[300px] h-[300px] rounded-full bg-[#eee]">
+      <div className="overflow-hidden  w-[300px] h-[300px] rounded-full bg-[#fff]">
         <Image
           alt="Speaker"
           src={img}
