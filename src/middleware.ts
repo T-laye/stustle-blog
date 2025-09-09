92ac42f62e03cec7ff0fcd94ffdfa006de0d9be1
Tubolayefa <tlayefaah@gmail.com>
2025-09-09 11:32:21 +0100

    add middleware rewrite for portfolio subdomain

--- src/middleware.ts
+++ src/middleware.ts
@@ -18,10 +18,42 @@ export function middleware(req: NextRequest) {
     return NextResponse.rewrite(url);
   }
 
+  // Rewrite portfolio.stustle.com/ → /portfolio
+  if (hostname === "portfolio.stustle.com" && url.pathname === "/") {
+    url.pathname = "/portfolio";
+    return NextResponse.rewrite(url);
+  }
+
   return NextResponse.next();
 }
 
 export const config = {
-  matcher: ['/',"/events/big-conference"],
+  matcher: ["/", "/events/big-conference", "/portfolio"],
 };
 
+// import { NextRequest, NextResponse } from "next/server";
+
+// export function middleware(req: NextRequest) {
+//   const hostname = req.headers.get("host") || "";
+//   const url = req.nextUrl;
+
+//   // Redirect www.stustle.com/events/big-conference → big.stustle.com/
+//   if (
+//     hostname === "www.stustle.com" &&
+//     url.pathname === "/events/big-conference"
+//   ) {
+//     return NextResponse.redirect("https://big.stustle.com");
+//   }
+
+//   // Rewrite big.stustle.com/ → /events/big-conference
+//   if (hostname === "big.stustle.com" && url.pathname === "/") {
+//     url.pathname = "/events/big-conference";
+//     return NextResponse.rewrite(url);
+//   }
+
+//   return NextResponse.next();
+// }
+
+// export const config = {
+//   matcher: ['/',"/events/big-conference"],
+// };
