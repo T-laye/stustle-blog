ee2c0afbe32a741c10d2cc8cc2c3c854758e0e7b
Tubolayefa <tlayefaah@gmail.com>
2025-11-12 15:34:29 +0100

    refactor: update text in RoadmapOverview component for clarity and engagement

--- src/components/community/RoadmapOverview.tsx
+++ src/components/community/RoadmapOverview.tsx
@@ -19,8 +19,8 @@ export default function RoadmapOverview() {
             delicately sourced by us.
           </h1>
           <p className="text-center lg:leading-[140%] text-xl mt-[16px] md:text-2xl lg:text-[36px] lg:mt-[40px]">
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Soluta,
-            distinctio magnam! Ad hic aut amet ipsum natus saepe cupiditate
+            Learn on your schedule. Get access to free learning resources and
+            grow with a community that seek to empower you.
           </p>
         </div>
       </div>
