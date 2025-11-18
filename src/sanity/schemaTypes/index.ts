56cae8e17c11d3d36d7b1d734aaa10e140afccbf
Tubolayefa <tlayefaah@gmail.com>
2025-11-18 15:24:37 +0100

    feat: add stustler schema type and integrate into schema definition

--- src/sanity/schemaTypes/index.ts
+++ src/sanity/schemaTypes/index.ts
@@ -3,7 +3,8 @@ import { post } from "./post";
 import { event } from "./event";
 import { review } from "./review";
 import { project } from "./project";
+import { stustler } from "./stustler";
 
 export const schema: { types: SchemaTypeDefinition[] } = {
-  types: [post, event, review, project],
+  types: [post, event, review, project, stustler],
 };
