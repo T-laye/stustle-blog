b477b33a6b405814d4e134d2e92e3b2a16c7d132
Tubolayefa <tlayefaah@gmail.com>
2026-05-11 18:07:30 +0100

    removed speakers

--- src/app/(event)/events/big-conference/page.tsx
+++ src/app/(event)/events/big-conference/page.tsx
@@ -1,7 +1,7 @@
 import Hero from "@/components/bigConference/Hero";
 import About from "../../../../components/bigConference/About";
 import WhatElse from "../../../../components/bigConference/WhatElse";
-import Speakers from "../../../../components/bigConference/Speakers";
+// import Speakers from "../../../../components/bigConference/Speakers";
 import Sponsors from "../../../../components/bigConference/Sponsors";
 import Faq from "../../../../components/bigConference/Faq";
 import CountDown from "../../../../components/bigConference/CountDown";
@@ -20,7 +20,7 @@ export default function Page() {
 			<CountDown />
 			<About />
 			<WhatElse />
-			<Speakers />
+			{/* <Speakers /> */}
 			<Tickets />
 			<Sponsors />
 			{/* <GenerateImage /> */}
